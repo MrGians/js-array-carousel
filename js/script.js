38f76d017293657e3ee9b1870d77c2ff87e57df9
MrGians <gianlumura97@gmail.com>
2022-06-27 14:44:26 +0200

    Milestone-1 Done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// TRACCIA
+// Creato un array contenente una lista di cinque immagini tra quelle fornite, creare un carosello.
+
+// MILESTONE 1
+// Creare il markup statico e lo stile per poterci poi concentrare solamente sull'aspetto logico.
+
+// MILESTONE 2
+// Rimuovere tutto il markup statico e inserire le immagini dinamicamente servendoci dell'array fornito e di un ciclo for.
+
+// MILESTONE 3
+// Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider al posto della precedente.
+
+// BONUS 1:
+// Aggiungere un ciclo infinito del carosello (Arrivati alla fine di una direzione, si ricomincia da capo dal rispettivo verso).
+
+// BONUS 2:
+// Creare delle miniature di tutte le immagni, in cui dovrà apparire in evidenza l’immagine equivalente a quella attiva.
+
