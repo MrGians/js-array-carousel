413b6575b3dc5db36b1b19378f0a2f79f0468b02
MrGians <gianlumura97@gmail.com>
2022-06-27 17:34:49 +0200

    Bonus 1 Done

---
+++ bonus 1/js/script.js
@@ -0,0 +1,84 @@
+// TRACCIA
+// Creato un array contenente una lista di cinque immagini tra quelle fornite, creare un carosello.
+
+// MILESTONE 1
+// Creare il markup statico e lo stile per poterci poi concentrare solamente sull'aspetto logico.
+
+// MILESTONE 2
+// Rimuovere tutto il markup statico e inserire le immagini dinamicamente servendoci dell'array fornito e di un ciclo for.
+
+// MILESTONE 3
+// Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider al posto della precedente.
+
+// BONUS 1:
+// Aggiungere un ciclo infinito del carosello (Arrivati alla fine di una direzione, si ricomincia da capo dal rispettivo verso).
+
+// BONUS 2:
+// Creare delle miniature di tutte le immagni, in cui dovrà apparire in evidenza l’immagine equivalente a quella attiva.
+
+
+
+// Variabili Globali
+const carouselGallery = document.getElementById("gallery");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+
+
+// Array contenente il percorso delle immagini
+const gallerySources = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+
+// Creo variabile di appoggio per immagine corrente
+let galleryIndex = 0;
+
+
+// Creo dinamicamente le immagini
+for(let i = 0; i < gallerySources.length; i++) {
+
+  // Creo l'elemento immagine
+  let images = `<img src="${gallerySources[i]}" alt="immagine-${i + 1}">`;
+    
+  // Stampo l'elemento immagine sul DOM ad ogni ciclo
+  carouselGallery.innerHTML += images;
+
+}
+
+
+// Seleziono tutti gli elementi IMG dal DOM ed al primo aggiungo la classe Active
+const gallery = document.querySelectorAll('img');
+gallery[galleryIndex].classList.add('active');
+
+
+// Evento di scorrimento al Click del bottone destro
+nextButton.addEventListener("click", function(){
+
+  // Rimuovo la classe Active dall'elemento immagine precedente
+  gallery[galleryIndex].classList.remove("active");
+
+  // Cambio immagine corrente tramite la variabile di appoggio
+  galleryIndex++;
+
+  // Verifica se ci sono ancora elementi nell'array, e se finiti ricomincia a scorrere dal primo
+  if(galleryIndex == gallerySources.length) {galleryIndex = 0}
+
+  // Aggiungo la classe al nuovo elemento
+  gallery[galleryIndex].classList.add("active");
+
+})
+
+
+// Evento di scorrimento al Click del bottone sinistro
+previousButton.addEventListener("click", function(){
+
+  // Rimuovo la classe Active dall'elemento immagine precedente
+  gallery[galleryIndex].classList.remove("active");
+
+  // Cambio immagine corrente tramite la variabile di appoggio
+  galleryIndex--;
+
+  // Verifica se ci sono ancora elementi nell'array, e se finiti ricomincia a scorrere dal primo
+  if(galleryIndex == -1) {galleryIndex = gallerySources.length - 1}
+
+  // Aggiungo la classe al nuovo elemento
+  gallery[galleryIndex].classList.add("active");
+
+})
